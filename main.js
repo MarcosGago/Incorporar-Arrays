c1a1559bf0e2b634ae527d595f7dfd2a5a852750
Ramiro <ramifazio@gmail.com>
2022-08-17 12:57:54 -0300

    Sugerencias D3!

--- main.js
+++ main.js
@@ -1,27 +1,24 @@
-class prendas{
-    constructor(tipo,precio,talle){
-        this.tipo=tipo
-        this.precio=precio
-        this.talle=talle
-    }
+class prendas {
+  constructor(tipo, precio, talle) {
+    this.tipo = tipo;
+    this.precio = precio;
+    this.talle = talle;
+  }
 }
 
-let prendas1 = new prendas("remera",1000,"m")
-let prendas2 = new prendas("pantalon",1400,"s")
-let prendas3 = new prendas("buzo",1200,"xl")
-let prendas4 = new prendas("calza",1500,"m")
+let prendas1 = new prendas("remera", 1000, "m");
+let prendas2 = new prendas("pantalon", 1400, "s");
+let prendas3 = new prendas("buzo", 1200, "xl");
+let prendas4 = new prendas("calza", 1500, "m");
 
-const ropas= []
+const ropas = [];
 
-ropas.push(prendas1)
-ropas.push(prendas2)
-ropas.push(prendas3)
-ropas.push(prendas4)
+ropas.push(prendas1, prendas2, prendas3, prendas4); //DRY the code ;) (Do Not Repeat Yourself)
 
-console.log(ropas)
+console.log(ropas);
 
-//filtrar por precio 
+//filtrar por precio
 
-let preciomenor1400 = ropas.filter(prendas =>prendas.precio <= 1400)
+let preciomenor1400 = ropas.filter((prendas) => prendas.precio <= 1400);
 
-console.log(preciomenor1400)
+console.log(preciomenor1400);
